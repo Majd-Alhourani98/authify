066b5ccd69efbddce8c62bcd808e054790623be6
Majd Alhourani <majd.aldein.alhournai@gmaail.com>
2025-12-10 07:43:52 -0800

    refactor: enhance error handler to differentiate between development and production

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,34 @@
+const sendErrordev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong. Please try again later',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrordev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
