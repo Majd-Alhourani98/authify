0ade87ee8a7f02f3e57ba4c020c9478c509132c4
Majd Alhourani <majd.aldein.alhournai@gmaail.com>
2025-12-10 07:33:21 -0800

    feat: add catchAsync utility to handle async errors in controllers

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,19 +1,15 @@
 const User = require('../models/user.model');
+const catchAsync = require('../utils/catchAsync');
 
-const signup = async (req, res) => {
+const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
-  try {
-    const user = await User.create({ name, email, password, passwordConfirm });
-    res.status(201).json({
-      status: 'success',
-      data: { user: user },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+
+  const user = await User.create({ name, email, password, passwordConfirm });
+
+  res.status(201).json({
+    status: 'success',
+    data: { user: user },
+  });
+});
 
 module.exports = { signup };
